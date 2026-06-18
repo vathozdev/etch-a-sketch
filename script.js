79cec13de2c75ee7fb4a0ad35cc78a91995bde92
oedipusdev <271198640+oedipusdev@users.noreply.github.com>
2026-06-18 13:15:36 +0300

    Implement grid creation logic in createGrid function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function createGrid(size) {
-    
+    let squareNumber = ( size * size );
+    for (let i = 0; i < squareNumber; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("square");
+        container.appendChild(newDiv);
+    }
 }
